1ed475ce5b24785864f5959c5708dddd9a21ca9a
Adrian Norberto Marino <adrian.marino@almundo.com>
2017-06-27 08:11:59 -0300

    threadlocal section

--- src/test/java/ThreadLocalTest.java
+++ src/test/java/ThreadLocalTest.java
@@ -1,11 +1,9 @@
 
-import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.text.NumberFormat;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.function.Function;
 
@@ -13,7 +11,7 @@ import static com.google.common.collect.Lists.newArrayList;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.fail;
 
 public class ThreadLocalTest {
@@ -24,7 +22,7 @@ public class ThreadLocalTest {
         List<Double> totals = newArrayList(1500.15D, 2500.15D, 3500.15D);
         ExecutorService service = newFixedThreadPool(3);
         ThreadLocal<NumberFormat> formatter = ThreadLocal.withInitial(NumberFormat::getCurrencyInstance);
-        Function<Future<String>, String> getValue = future -> {
+        Function<Future<String>, String> getFutureResult = future -> {
             try {
                 return future.get();
             } catch (Exception exception) {
@@ -34,16 +32,12 @@ public class ThreadLocalTest {
         };
 
         // Perform
-        List<String> stringTotals = totals
-                .stream()
+        List<String> stringTotals = totals.stream()
                 .map(total -> service.submit(() -> formatter.get().format(total)))
-                .map(getValue)
-                .collect(toList());
+                .map(getFutureResult).collect(toList());
 
         // Asserts
-        assertThat(stringTotals, hasItem("$1,500.15"));
-        assertThat(stringTotals, hasItem("$2,500.15"));
-        assertThat(stringTotals, hasItem("$3,500.15"));
+        assertThat(stringTotals, hasItems("$1,500.15", "$2,500.15", "$3,500.15"));
     }
 
     @Test
@@ -53,14 +47,10 @@ public class ThreadLocalTest {
         ThreadLocal<NumberFormat> formatter = ThreadLocal.withInitial(NumberFormat::getCurrencyInstance);
 
         // Perform
-        List<String> stringTotals = totals
-                .parallelStream()
-                .map(total -> formatter.get().format(total))
-                .collect(toList());
+        List<String> stringTotals = totals.parallelStream()
+                .map(total -> formatter.get().format(total)).collect(toList());
 
         // Asserts
-        assertThat(stringTotals, hasItem("$1,500.15"));
-        assertThat(stringTotals, hasItem("$2,500.15"));
-        assertThat(stringTotals, hasItem("$3,500.15"));
+        assertThat(stringTotals, hasItems("$1,500.15", "$2,500.15", "$3,500.15"));
     }
 }
